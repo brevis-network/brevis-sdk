06efc9ff4d84da18dd7494f8eb06e55afc32c021
patrickmao <patrickmao93@gmail.com>
2024-02-08 19:06:12 +0800

    change batch size to 512

--- sdk/circuit_input.go
+++ sdk/circuit_input.go
@@ -113,7 +113,7 @@ func (dp DataPoints[T]) Clone() DataPoints[T] {
 // NumMaxDataPoints is the max amount of data points this circuit can handle at
 // once. This couples tightly to the batch size of the aggregation circuit on
 // Brevis' side
-const NumMaxDataPoints = 100
+const NumMaxDataPoints = 512
 
 // NumMaxLogFields is the max amount of log fields each Receipt can have. This
 // couples tightly to the decoding capacity of the receipt decoder circuit on
