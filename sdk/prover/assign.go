fd1e97e994ccde14f067d3158bd8d9218261ea3c
patrickmao <patrickmao93@gmail.com>
2024-04-02 15:41:51 +0800

    working

--- sdk/prover/assign.go
+++ sdk/prover/assign.go
@@ -3,8 +3,20 @@ package prover
 import (
 	"github.com/brevis-network/brevis-sdk/sdk"
 	"github.com/brevis-network/brevis-sdk/sdk/proto/sdkproto"
+	"reflect"
 )
 
-func assignCustomInput(app sdk.AppCircuit, input *sdkproto.CustomInput) sdk.AppCircuit {
-	return nil
+func assignCustomInput(app sdk.AppCircuit, input *sdkproto.CustomInput) (sdk.AppCircuit, error) {
+	vv := reflect.ValueOf(app)
+
+	// deref until we get the actual value
+	for vv.Kind() == reflect.Pointer {
+		vv = vv.Elem()
+	}
+
+	if vv.Kind() != reflect.Struct {
+
+	}
+
+	return nil, nil
 }
