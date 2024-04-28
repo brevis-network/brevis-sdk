b3c6c46e438ce41695517325953e0aefe7a480db
jinyao <1650091775@qq.com>
2024-04-28 11:10:06 +0800

    Revert "fix receipt logs auto padding"
    This reverts commit a95c541b460db2afa0263220c4c0c1a2ee84259f.

--- sdk/app.go
+++ sdk/app.go
@@ -473,18 +473,13 @@ func (q *BrevisApp) assignReceipts(in *CircuitInput) error {
 
 func buildLogFields(fs [NumMaxLogFields]LogFieldData) (fields [NumMaxLogFields]LogField) {
 	empty := LogFieldData{}
-
-	lastNonEmpty := fs[0]
-	for i := 0; i < NumMaxLogFields; i++ {
+	for i, f := range fs {
 		// Due to backend circuit's limitations, we must fill []LogField with valid data
 		// up to NumMaxLogFields. If the user actually only wants less NumMaxLogFields
 		// log fields, then we simply copy the previous log field in the list to fill the
 		// empty spots.
-		f := fs[i]
 		if i > 0 && f == empty {
-			f = lastNonEmpty
-		} else {
-			lastNonEmpty = f
+			f = fs[i-1]
 		}
 		fields[i] = LogField{
 			Contract: ConstUint248(f.Contract),
