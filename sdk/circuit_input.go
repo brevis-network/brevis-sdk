888fe2f7319c11b04d8e6b13857cd96a0e291e78
jinyao <1650091775@qq.com>
2024-04-29 11:37:38 +0800

    use 512 as NumMaxDataPoints

--- sdk/circuit_input.go
+++ sdk/circuit_input.go
@@ -134,7 +134,7 @@ func (dp DataPoints[T]) Clone() DataPoints[T] {
 // NumMaxDataPoints is the max amount of data points this circuit can handle at
 // once. This couples tightly to the batch size of the aggregation circuit on
 // Brevis' side
-const NumMaxDataPoints = 1500
+const NumMaxDataPoints = 512
 
 // NumMaxLogFields is the max amount of log fields each Receipt can have. This
 // couples tightly to the decoding capacity of the receipt decoder circuit on
