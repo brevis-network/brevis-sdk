2e3298edde83763088b66df9eec54e3a9c549b5f
liuxiaobleach <1241368737@qq.com>
2024-09-03 19:49:25 +0800

    add plonky2 bn254 vk hash

--- sdk/periphery.go
+++ sdk/periphery.go
@@ -16,7 +16,7 @@ import (
 	cs_bn254 "github.com/consensys/gnark/constraint/bn254"
 	"github.com/consensys/gnark/frontend"
 	"github.com/consensys/gnark/frontend/cs/scs"
-	"github.com/consensys/gnark/std/algebra/native/sw_bls12377"
+	"github.com/consensys/gnark/std/algebra/emulated/sw_bn254"
 	replonk "github.com/consensys/gnark/std/recursion/plonk"
 	"github.com/consensys/gnark/test/unsafekzg"
 	"github.com/ethereum/go-ethereum/common"
@@ -118,13 +118,12 @@ func printVkHash(vk plonk.VerifyingKey) {
 }
 
 func ComputeVkHash(vk plonk.VerifyingKey) (common.Hash, error) {
-	plonkCircuitVk, err := replonk.ValueOfVerifyingKey[sw_bls12377.ScalarField, sw_bls12377.G1Affine, sw_bls12377.G2Affine](vk)
+	plonkCircuitVk, err := replonk.ValueOfVerifyingKey[sw_bn254.ScalarField, sw_bn254.G1Affine, sw_bn254.G2Affine](vk)
 	if err != nil {
 		return common.Hash{}, err
 	}
 
-	appVkHash761 := utils.CalculateAppVkHashFor761(plonkCircuitVk)
-	appVkHash := utils.CalculateAppVkHashFrom761To254(appVkHash761)
+	appVkHash := utils.CalculateAppVkHashForBn254(plonkCircuitVk)
 	return common.BytesToHash(appVkHash), nil
 }
 
