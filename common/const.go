7c73fc137908aac63e7c39320dee358c4b8ca9cf
jinyao <1650091775@qq.com>
2024-10-16 15:05:13 +0800

    fix compilation error

--- common/const.go
+++ common/const.go
@@ -1,7 +1,7 @@
 package common
 
 var DummyReceiptInputCommitment = map[uint64]string{
-	1: "0x2c5e0beb9dac6b77e64945195f455c6d4eea855c98b16c2bbdf0f5908b5051d5",
+	1: "0x127d5d805cfd68acd5c88659d1cf96bcec545959ed27b8d654e90a8d9165501d",
 }
 
 var DummyStorageInputCommitment = map[uint64]string{
@@ -9,5 +9,5 @@ var DummyStorageInputCommitment = map[uint64]string{
 }
 
 var DummyTransactionInputCommitment = map[uint64]string{
-	1: "0x2db248500f85132995f4d45c82ca8d70a0cecd90e62fae79007260b97c96c350",
+	1: "0xe4dda4409fbd8fd88485c70167bc1cfe55753b147aeb1725a2dee8aa59ba92",
 }
