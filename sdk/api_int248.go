0ff15bd3bc9732684f6613119de649cff90da543
grrThea <anran@celer.network>
2024-10-20 20:16:24 +0800

    ABS function optimization

--- sdk/api_int248.go
+++ sdk/api_int248.go
@@ -168,16 +168,25 @@ func (api *Int248API) IsZero(a Int248) Uint248 {
 }
 
 // ABS returns the absolute value of a
+// func (api *Int248API) ABS(a Int248) Uint248 {
+// 	bs := api.ToBinary(a)
+// 	signBit := bs[247] // ToBinary returns little-endian bits, the last bit is sign
+// 	flipped := make([]frontend.Variable, len(bs))
+// 	for i, v := range bs {
+// 		flipped[i] = api.g.IsZero(v.Val)
+// 	}
+// 	absWhenOrigIsNeg := api.g.Add(1, api.g.FromBinary(flipped...))
+// 	abs := api.g.Select(signBit.Val, absWhenOrigIsNeg, a.Val)
+// 	return newU248(abs)
+// }
+
+// ABS returns the absolute value of a
 func (api *Int248API) ABS(a Int248) Uint248 {
-	bs := api.ToBinary(a)
-	signBit := bs[247] // ToBinary returns little-endian bits, the last bit is sign
-	flipped := make([]frontend.Variable, len(bs))
-	for i, v := range bs {
-		flipped[i] = api.g.IsZero(v.Val)
-	}
-	absWhenOrigIsNeg := api.g.Add(1, api.g.FromBinary(flipped...))
-	abs := api.g.Select(signBit.Val, absWhenOrigIsNeg, a.Val)
-	return newU248(abs)
+	a = api.ensureSignBit(a)
+	resultIfNonNeg := a.Val
+	resultIfNeg := api.g.Sub(new(big.Int).Lsh(big.NewInt(1), 248), a.Val)
+	result := api.g.Select(a.SignBit, resultIfNeg, resultIfNonNeg)
+	return newU248(result)
 }
 
 //func (api *Int248API) Add(a, b Int248) Int248 {
