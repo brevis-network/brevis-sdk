8764bf09dbb95ce7025433b5bfb3b76eb4474df6
jinyao <1650091775@qq.com>
2024-10-21 16:37:42 +0800

    use actual bn254 srs

--- sdk/periphery.go
+++ sdk/periphery.go
@@ -10,6 +10,7 @@ import (
 	"time"
 
 	"github.com/brevis-network/brevis-sdk/common/utils"
+	"github.com/brevis-network/brevis-sdk/sdk/srs"
 	"github.com/consensys/gnark-crypto/ecc"
 	"github.com/consensys/gnark/backend/plonk"
 	"github.com/consensys/gnark/backend/witness"
@@ -20,7 +21,6 @@ import (
 	"github.com/consensys/gnark/frontend/cs/scs"
 	"github.com/consensys/gnark/std/algebra/emulated/sw_bn254"
 	replonk "github.com/consensys/gnark/std/recursion/plonk"
-	"github.com/consensys/gnark/test/unsafekzg"
 	"github.com/ethereum/go-ethereum/common"
 )
 
@@ -83,14 +83,9 @@ func Setup(ccs constraint.ConstraintSystem, cacheDir string, maxReceipt, maxStor
 		return nil, nil, nil, fmt.Errorf("must provide a directory to save SRS")
 	}
 	r1cs := ccs.(*cs.SparseR1CS)
-	// srsDir := os.ExpandEnv(cacheDir)
+	srsDir := os.ExpandEnv(cacheDir)
 
-	// canonical, lagrange, err := srs.NewSRS(r1cs, "https://kzg-srs.s3.us-west-2.amazonaws.com", srsDir)
-	// if err != nil {
-	// 	return
-	// }
-
-	canonical, lagrange, err := unsafekzg.NewSRS(r1cs)
+	canonical, lagrange, err := srs.NewSRS(r1cs, "https://trusted-setup-halo2kzg.s3.eu-central-1.amazonaws.com", srsDir)
 	if err != nil {
 		return
 	}
