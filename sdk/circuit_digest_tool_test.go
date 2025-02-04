1d2be7d460e9e74795b21a5993eb2f983d96ac23
jinyao <1650091775@qq.com>
2025-02-04 10:00:36 +0800

    update brevis proto

--- sdk/circuit_digest_tool_test.go
+++ sdk/circuit_digest_tool_test.go
@@ -24,7 +24,7 @@ var testBrevisApp = &BrevisApp{
 
 func TestHash2HashCircuitDigest(t *testing.T) {
 	assert := test.NewAssert(t)
-	digest, err := GetHash2HashCircuitDigest(32, 32, 64)
+	digest, err := GetHash2HashCircuitDigest(32, 32, 64, NewBrevisAppWithDigestsSetOnlyFromRemote())
 	assert.NoError(err)
 	log.Infof("digest: %x", digest)
 	log.Infof("digest: %s", digest)
@@ -87,7 +87,7 @@ func TestHashAllDigest(t *testing.T) {
 	log.Infof("app vk hashL %s", appVkHashBigInt)
 	log.Infof("app vk hashL %x", appVkHashBigInt)
 
-	hash2HashDigest, err := GetHash2HashCircuitDigest(32, 32, 64)
+	hash2HashDigest, err := GetHash2HashCircuitDigest(32, 32, 64, testBrevisApp)
 	assert.NoError(err)
 
 	plonky2RootFromBn128Digest, _, _, err := GetPlonky2CircuitDigestFromRootNodeSelf(32, 32, 64, testBrevisApp)
